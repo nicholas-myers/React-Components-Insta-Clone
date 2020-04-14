eadfd65f1e2ffb81426ded90de2ba5d122c99270
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-14 15:00:04 -0700

    got likes to increase when clicking the heart

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,12 +1,16 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const LikeSection = ({likes}) => {
-
+const [numberOfLikes, setNumberOfLikes] = useState(likes) 
+  const getPopular = (event) => {
+    setNumberOfLikes(numberOfLikes + 1)
+    event.target.style.color = "red"
+  }
 
   return (
     <div>
@@ -15,13 +19,13 @@ const LikeSection = ({likes}) => {
         key="likes-icons-container"
       >
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={getPopular} />
         </div>
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
+          <FontAwesomeIcon icon={faComment}  />
         </div>
       </div>
-  <p className="like-number">{likes}</p>
+  <p className="like-number">{numberOfLikes}</p>
     </div>
   )
 };
